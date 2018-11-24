ddfd562f49b5c9f303001cca652631c0b54c4b77
Vlad <gimrcpp@gmail.com>
2018-11-24 15:16:48 +0300

    changes

--- README.md
+++ README.md
@@ -3,8 +3,6 @@
     <img src="https://img.shields.io/badge/paypal-donate-green.svg" alt="PayPal donate button"/>
 </a>
 
-Run command and start doing new project faster
-
 ```
 git clone https://github.com/androidovshchik/AndroidBlankProject.git NewProjectName
 ```
--- app/src/main/java/io/androidovshchik/project/extensions/EditText.kt
+++ app/src/main/java/io/androidovshchik/project/extensions/EditText.kt
@@ -9,16 +9,21 @@ package io.androidovshchik.project.extensions
 import android.text.InputFilter
 import android.text.InputType
 import android.text.method.DigitsKeyListener
-import android.widget.TextView
+import android.widget.EditText
 
-fun TextView.maxLength(maxLength: Int) {
+fun EditText.maxLength(maxLength: Int) {
     val array = arrayOfNulls<InputFilter>(1)
     array[0] = InputFilter.LengthFilter(maxLength)
     filters = array
 }
 
-fun TextView.onlyNumbers() {
+fun EditText.onlyNumbers() {
     inputType = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_DECIMAL or
         InputType.TYPE_NUMBER_FLAG_SIGNED
     keyListener = DigitsKeyListener.getInstance("0123456789")
+}
+
+fun EditText.onlyUppercase() {
+    inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_CAP_CHARACTERS
+    filters = arrayOf(InputFilter.AllCaps())
 }
